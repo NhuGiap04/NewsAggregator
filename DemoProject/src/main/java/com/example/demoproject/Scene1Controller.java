8897f8f94ef69961cbd5acb46d5c443d55aedbd6
AndrewNguyen4 <153667379+AndrewNguyen4@users.noreply.github.com>
2024-05-20 13:46:15 +0700

    Add files via upload
    Make Scene1 extend ArticleDisplay

--- DemoProject/src/main/java/com/example/demoproject/Scene1Controller.java
+++ DemoProject/src/main/java/com/example/demoproject/Scene1Controller.java
@@ -1,5 +1,7 @@
 package com.example.demoproject;
 
+import com.example.demoproject.article.Article;
+import com.example.demoproject.article.ArticleCardController;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,29 +15,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class Scene1Controller extends Scenes implements Initializable {
+public class Scene1Controller extends ArticleDisplay implements Initializable {
 
     @FXML
     private Button userGuide;
-    @FXML
-    VBox articlesLayout;
+
     List<Article> recentArticles;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         recentArticles = new ArrayList<>(recentArticles());
-        try {
-            for (Article recentArticle : recentArticles) {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("articleCard.fxml"));
-                HBox box = fxmlLoader.load();
-                ArticleCardController articleCardController = fxmlLoader.getController();
-                articleCardController.setData(recentArticle);
-                articlesLayout.getChildren().add(box);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        displayResult(recentArticles);
     }
     private List<Article> recentArticles(){
         List<Article> list = new ArrayList<>();
