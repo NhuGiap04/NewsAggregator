52af06c4147185e8039e5fa266b3d53a8ddd96d1
brandon-mai <anhbinhbao0@gmail.com>
2024-05-20 20:37:39 +0700

    add 2 cases for activting venv

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 cd API-Embed
 python -m venv venv
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    .venv/Scripts/activate
+else
+    source venv/bin/activate
+fi
 pip install --no-cache-dir -r requirements.txt
-activate
+
 
 cd ../API-Search
 mvn clean install
